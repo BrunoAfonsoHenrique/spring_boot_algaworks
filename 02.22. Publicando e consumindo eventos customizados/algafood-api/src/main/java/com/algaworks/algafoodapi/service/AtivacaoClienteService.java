d7cde4d58d3318654af1d466ed78ef202413490a
Bruno <bruno_afonso_henrique@outlook.com>
2023-12-12 14:29:22 -0300

    codigo fonte da aula

---
+++ 02.22. Publicando e consumindo eventos customizados/algafood-api/src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafoodapi.service;
+
+import com.algaworks.algafoodapi.model.Cliente;
+import com.algaworks.algafoodapi.notificacao.NivelUrgencia;
+import com.algaworks.algafoodapi.notificacao.Notificador;
+import com.algaworks.algafoodapi.notificacao.TipoDoNotificador;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+    @TipoDoNotificador(NivelUrgencia.URGENTE)
+    @Autowired
+    private Notificador notificador;
+    
+    public void ativar(Cliente cliente) {
+        cliente.ativar();
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+    }
+
+}
