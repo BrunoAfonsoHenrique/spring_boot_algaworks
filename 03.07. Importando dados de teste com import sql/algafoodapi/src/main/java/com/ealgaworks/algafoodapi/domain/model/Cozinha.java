bd48f93313d789c140a4b890d4a646a9a25264c6
Bruno <bruno_afonso_henrique@outlook.com>
2024-01-03 22:18:33 -0300

    codigo fonte da aula

---
+++ 03.07. Importando dados de teste com import sql/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/domain/model/Cozinha.java
@@ -0,0 +1,47 @@
+package com.ealgaworks.algafoodapi.domain.model;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Cozinha")
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
