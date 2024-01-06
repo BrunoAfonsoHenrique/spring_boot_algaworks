7bef464cd5774232ec129497d6afb34c34a70b11
Bruno <bruno_afonso_henrique@outlook.com>
2024-01-06 11:31:30 -0300

    codigo fonte da aula

---
+++ 03.09. Adicionando um objeto no banco de dados/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,29 @@
+package com.ealgaworks.algafoodapi.jpa;
+
+import com.ealgaworks.algafoodapi.AlgafoodapiApplication;
+
+import com.ealgaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodapiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext
+                .getBean(CadastroCozinha.class);
+
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+        for (Cozinha cozinha : cozinhas) {
+            System.out.println(cozinha.getNome());
+        }
+    }
+
+}
