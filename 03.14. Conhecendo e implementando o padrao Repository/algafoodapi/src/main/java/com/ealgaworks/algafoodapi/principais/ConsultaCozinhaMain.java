f77ea302cac59379f7c384e249650c74a022533d
Bruno <bruno_afonso_henrique@outlook.com>
2024-01-11 19:48:51 -0300

    codigo fonte da aula

---
+++ 03.14. Conhecendo e implementando o padrao Repository/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/principais/ConsultaCozinhaMain.java
@@ -0,0 +1,30 @@
+package com.ealgaworks.algafoodapi.principais;
+
+import com.ealgaworks.algafoodapi.AlgafoodapiApplication;
+
+import com.ealgaworks.algafoodapi.domain.model.Cozinha;
+import com.ealgaworks.algafoodapi.jpa.CadastroCozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodapiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext
+                .getBean(CadastroCozinha.class);
+
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+        for (Cozinha cozinha : cozinhas) {
+            System.out.println(cozinha.getNome());
+        }
+    }
+
+}
