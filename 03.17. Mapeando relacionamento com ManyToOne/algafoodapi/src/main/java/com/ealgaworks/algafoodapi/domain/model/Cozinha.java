ebd6b041e51c9c8cc5b268c7985a2b06cec0d9e9
Bruno <bruno_afonso_henrique@outlook.com>
2024-01-13 16:47:55 -0300

    codigo fonte da aula

---
+++ 03.17. Mapeando relacionamento com ManyToOne/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/domain/model/Cozinha.java
@@ -0,0 +1,25 @@
+package com.ealgaworks.algafoodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Cozinha")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+}
---
+++ 03.17. Mapeando relacionamento com ManyToOne/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/principais/InclusaoCozinhaMain.java
@@ -0,0 +1,33 @@
+package com.ealgaworks.algafoodapi.principais;
+
+import com.ealgaworks.algafoodapi.AlgafoodapiApplication;
+import com.ealgaworks.algafoodapi.domain.model.Cozinha;
+import com.ealgaworks.algafoodapi.repository.CozinhaRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class InclusaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodapiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CozinhaRepository cozinhaRepository = applicationContext
+                .getBean(CozinhaRepository.class);
+
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Brasileira");
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Japonesa");
+
+        cozinha1 = cozinhaRepository.salvar(cozinha1);
+        cozinha2 = cozinhaRepository.salvar(cozinha2);
+
+        System.out.printf("%d - %s\n",cozinha1.getId(), cozinha1.getNome());
+        System.out.printf("%d - %s\n",cozinha2.getId(), cozinha2.getNome());
+    }
+
+}
