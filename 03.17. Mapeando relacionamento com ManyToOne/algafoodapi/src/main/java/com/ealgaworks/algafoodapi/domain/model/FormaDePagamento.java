a2cbf65cac0bc614b792725406b3dc9ba5917ca4
Bruno <bruno_afonso_henrique@outlook.com>
2024-01-28 13:44:51 -0300

    codigo fonte da aula

---
+++ 03.17. Mapeando relacionamento com ManyToOne/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/domain/model/FormaDePagamento.java
@@ -0,0 +1,4 @@
+package com.ealgaworks.algafoodapi.domain.model;
+
+public class FormaDePagamento {
+}
---
+++ 03.20. Desafio mapeando entidades/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/repository/CozinhaRepository.java
@@ -0,0 +1,16 @@
+package com.ealgaworks.algafoodapi.repository;
+
+import com.ealgaworks.algafoodapi.domain.model.Cozinha;
+
+import java.util.List;
+
+public interface CozinhaRepository {
+
+    List<Cozinha> listar();
+
+    Cozinha buscarPorId(Long id);
+
+    Cozinha salvar(Cozinha cozinha);
+
+    void remover(Cozinha cozinha);
+}
