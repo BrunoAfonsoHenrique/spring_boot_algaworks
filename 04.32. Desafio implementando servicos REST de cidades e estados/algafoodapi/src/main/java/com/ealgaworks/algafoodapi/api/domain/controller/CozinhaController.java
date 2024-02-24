9efe2abee2cb35e60cae1a2b0990e03bb7a644cb
Bruno <bruno_afonso_henrique@outlook.com>
2024-02-24 12:50:05 -0300

    codigo fonte da aula

---
+++ 04.32. Desafio implementando servicos REST de cidades e estados/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/api/domain/controller/CozinhaController.java
@@ -0,0 +1,72 @@
+package com.ealgaworks.algafoodapi.api.domain.controller;
+
+import com.ealgaworks.algafoodapi.api.domain.model.Cozinha;
+import com.ealgaworks.algafoodapi.api.domain.service.CozinhaService;
+import com.ealgaworks.algafoodapi.api.domain.exceptions.EntidadeEmUsoException;
+import com.ealgaworks.algafoodapi.api.domain.exceptions.EntidadeNaoEncontradaException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaService cozinhaService;
+
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaService.listarTodasCozinhas();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+
+        try {
+            Cozinha cozinha = cozinhaService.buscarPorId(cozinhaId);
+            return ResponseEntity.status(HttpStatus.FOUND).body(cozinha);
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping("/incluir-nova-cozinha")
+    public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha) {
+        Cozinha cozinhaSalva = cozinhaService.salvar(cozinha);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cozinhaSalva);
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long id,
+                                             @RequestBody Cozinha cozinha) {
+        try {
+            cozinhaService.atualizarCozinha(id, cozinha);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Cozinha> deletar(@PathVariable Long id) {
+        try {
+            cozinhaService.excluir(id);
+            return ResponseEntity.status(HttpStatus.OK).build();
+
+        } catch (EntidadeEmUsoException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+}
