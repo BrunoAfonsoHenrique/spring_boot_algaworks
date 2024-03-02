6a352544d65a69cf769d5d8048b326e887a85112
Bruno <bruno_afonso_henrique@outlook.com>
2024-03-02 09:14:48 -0300

    codigo fonte da aula

--- 02.15. Dependencia opcional com Autowired/injecao-dependencia/src/main/java/com/algaworks/injecaodependencia/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.injecaodependencia.notificacao;
-
-import com.algaworks.injecaodependencia.model.Cliente;
-
-public interface Notificador {
-    void notificar(Cliente cliente, String mensagem);
-}
