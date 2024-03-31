9eecf7556ea2061a3579e0ebe9d389c3102c9f8f
Bruno <bruno_afonso_henrique@outlook.com>
2024-03-31 10:19:21 -0300

    codigo fonte da aula

---
+++ 06.02. Relacionamento muitos-para-muitos - ManyToMany/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/api/domain/controller/TesteController.java
@@ -0,0 +1,91 @@
+package com.ealgaworks.algafoodapi.api.domain.controller;
+
+import com.ealgaworks.algafoodapi.api.domain.infrastructure.repository.specification.RestaurantesSpecifications;
+import com.ealgaworks.algafoodapi.api.domain.model.Cozinha;
+import com.ealgaworks.algafoodapi.api.domain.model.Restaurante;
+import com.ealgaworks.algafoodapi.api.domain.repository.CozinhaRepository;
+import com.ealgaworks.algafoodapi.api.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> consultarPorNome(@RequestParam("nome") String nome) {
+        // List<Cozinha> cozinha = cozinhaRepository.nome(nome);
+
+        // List<Cozinha> cozinha = cozinhaRepository.findTodasByNome(nome);
+
+        List<Cozinha> cozinha = cozinhaRepository.findTodasByNomeContaining(nome);
+        return cozinha;
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome) {
+        // List<Cozinha> cozinha = cozinhaRepository.nome(nome);
+
+        Optional<Cozinha> cozinha = cozinhaRepository.findByNome(nome);
+        return cozinha;
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> listarTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome")
+    public List<Restaurante> listarTaxaFrete(String nome, Long cozinhaId) {
+        return restauranteRepository.consultarPorNome(nome, cozinhaId);
+    }
+
+    @GetMapping("/restaurantes/primeiro-por-nome")
+    public Optional<Restaurante> restaurantePrimeiroPorNome(String nome) {
+        return restauranteRepository.findFirstRestauranteByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/top2-por-nome")
+    public List<Restaurante> restauranteTop2PorNome(String nome) {
+        return restauranteRepository.findTop2ByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/por-nome-e-frete")
+    public List<Restaurante> restaurantesPorNomeFrete(String nome,
+                                                      BigDecimal taxaFreteInicial,
+                                                      BigDecimal taxaFreteFinal) {
+
+        return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+
+    }
+
+    @GetMapping("/restaurantes/com-frete-gratis")
+    public List<Restaurante> restaurantesComFreteGratis(String nome) {
+
+        return restauranteRepository.findComFreteGratis(nome);
+
+    }
+
+    @GetMapping("/restaurantes/primeiro")
+    public Optional<Restaurante> restaurantesPrimeiro() {
+        return restauranteRepository.buscarPrimeiro();
+    }
+
+    @GetMapping("/cozinhas/primeiro")
+    public Optional<Cozinha> cozinhasPrimeiro() {
+        return cozinhaRepository.buscarPrimeiro();
+    }
+}
---
+++ 06.02. Relacionamento muitos-para-muitos - ManyToMany/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/api/domain/repository/RestauranteRepository.java
@@ -0,0 +1,32 @@
+package com.ealgaworks.algafoodapi.api.domain.repository;
+
+import com.ealgaworks.algafoodapi.api.domain.model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public interface RestauranteRepository
+        extends CustomJpaRepository<Restaurante, Long>,
+        RestauranteRepositoryQueries,
+        JpaSpecificationExecutor<Restaurante> {
+
+    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+//    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id") // Aqui está comentado porque a consulta JPQL foi externalizada (META-INF > orm.xml)
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
+
+//    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+
+    Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+
+    List<Restaurante> findTop2ByNomeContaining(String nome);
+
+    int countByCozinhaId(Long cozinha);
+
+    
+}
