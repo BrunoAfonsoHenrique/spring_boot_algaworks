b44e8d92c0891c63225fc7046f25002417ebaabf
Bruno <bruno_afonso_henrique@outlook.com>
2024-05-01 12:07:55 -0300

    codigo fonte da aula

--- 06.10. Entendendo o Eager Loading/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ 06.10. Entendendo o Eager Loading/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -3,12 +3,16 @@ package com.algaworks.algafood.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends CustomJpaRepository<Cozinha, Long> {
+
+	@Query(nativeQuery = true, value = "SELECT * FROM cozinha")
+	List<Cozinha> findAll();
 
 	List<Cozinha> findTodasByNomeContaining(String nome);
 	
