3db0c24779220f612e9bae1739c6fd67d1f90434
Bruno <bruno_afonso_henrique@outlook.com>
2024-05-30 19:01:45 -0300

    codigo fonte da aula

--- 04.29. Desafio - modelando e implementando a consulta de recursos de restaurantes/algafoodapi/src/main/java/com/ealgaworks/algafoodapi/api/domain/exceptions/EntidadeEmUsoException.java
+++
@@ -1,9 +0,0 @@
-package com.ealgaworks.algafoodapi.api.domain.exceptions;
-
-public class EntidadeEmUsoException extends RuntimeException{
-
-    public EntidadeEmUsoException(String message) {
-        super(message);
-    }
-
-}
