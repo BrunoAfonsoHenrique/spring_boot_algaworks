4420b89a793f83eff05b60e48ba1bbe8c91530f8
BrunoAfonsoHenrique <bruno_afonso_henrique@outlook.com>
2025-01-12 19:05:33 -0300

    codigo fonte da aula

---
+++ 08.03. Lanccando excecoes do tipo ResponseStatusException/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,53 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Endereco enderecoEntrega;
+
+    private StatusPedido status;
+
+    private BigDecimal subTotal;
+    private BigDecimal taxaFrete;
+    private BigDecimal valorTotal;
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    private LocalDateTime dataConfirmacao;
+    private LocalDateTime dataCancelamento;
+    private LocalDateTime dataEntrega;
+
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FormaPagamento formaPagamento;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_cliente_id", nullable = false)
+    private Usuario cliente;
+
+
+
+}
+
